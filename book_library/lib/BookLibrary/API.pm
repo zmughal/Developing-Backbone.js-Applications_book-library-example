37edbc6aefb3562597131a1ba1f761de4435f9b7
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-07-21 17:40:13 -0500

    DB connection

--- book_library/lib/BookLibrary/API.pm
+++ book_library/lib/BookLibrary/API.pm
@@ -2,9 +2,17 @@ package BookLibrary::API;
 use Mojo::Base 'Mojolicious::Controller';
 
 use strict;
+use BookLibrary::Schema;
+use Path::Class;
+
+has db => sub {
+	my $sqlite_db = file(__FILE__)
+		->parent->parent->parent->file('prod.db')
+		->absolute;
+	BookLibrary::Schema->connect('dbi:SQLite:dbname='.$sqlite_db);
+};
 
 sub all_books {
-
 }
 
 sub get_book {
