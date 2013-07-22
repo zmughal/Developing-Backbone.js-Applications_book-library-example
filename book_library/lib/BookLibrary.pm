dbdd3fb124d7a3737854dbbe01d6c0cfb5e8d84a
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-07-21 23:43:38 -0500

    fix routes

--- book_library/lib/BookLibrary.pm
+++ book_library/lib/BookLibrary.pm
@@ -3,6 +3,8 @@ use Mojo::Base 'Mojolicious';
 
 use strict;
 
+use BookLibrary::API;
+
 # This method will run once at server start
 sub startup {
 	my $self = shift;
@@ -10,18 +12,22 @@ sub startup {
 	# Router
 	my $r = $self->routes;
 
+	my $api = $r->under('/api');
+
+	#my @id_match = ( id =>  qr/\d+/ ); # id parameter must be a number
+	my @id_match = ();
 	# Normal route to controller
 	# url             HTTP Method  Operation
 	# /api/books      GET          Get an array of all books
-	$r->get('/api/books')->to('api#all_books');
+	$api->route('/books', @id_match)->via('GET')->to('API#all_books');
 	# /api/books/:id  GET          Get the book with id of :id
-	$r->get('/api/books/:id')->to('api#get_book');
+	$api->route('/books/:id', @id_match)->via('GET')->to('API#get_book');
 	# /api/books      POST         Add a new book and return the book with an id attribute added
-	$r->post('/api/books/:id')->to('api#new_book');
+	$api->route('/books', @id_match)->via('POST')->to('API#new_book');
 	# /api/books/:id  PUT          Update the book with id of :id
-	$r->put('/api/books/:id')->to('api#update_book');
+	$api->route('/books/:id', @id_match)->via('PUT')->to('API#update_book');
 	# /api/books/:id  DELETE       Delete the book with id of :id
-	$r->delete('/api/books/:id')->to('api#delete_book');
+	$api->route('/books/:id', @id_match)->via('DELETE')->to('API#delete_book');
 }
 
 1;
